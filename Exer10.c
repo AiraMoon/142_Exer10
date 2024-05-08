4df582457e3a2209d70583325c96bbc27a521349
airamoon <mhmoon@up.edu.ph>
2024-05-08 22:41:16 +0800

    exer10-edited

--- Exer10.c
+++ Exer10.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<malloc.h>
 #define N 10
 #define BALANCED 0
@@ -99,16 +100,16 @@ void insert_fixup(avl_node **rootptr, avl_node *temp){
 				if(previous==LEFT_LEANING)
 					right_rotate(&temp);
 				else{
-					left_rotate(&(temp->left));
 					right_rotate(&temp);
+					left_rotate(&(temp->left));
 				}
 			}
 			else{
 				if(previous==RIGHT_LEANING)
 					left_rotate(&temp);
 				else{
-					right_rotate(&(temp->right));
 					left_rotate(&temp);
+					right_rotate(&(temp->right));
 				}
 			}
 		} //end if
@@ -160,9 +161,84 @@ void swap(int *a, int *b){
 	temp = *a; *a= *b; *b =temp;
 }
 
+void delhelper(avl_node **rootptr, avl_node **temp){
+	avl_node *del = *temp;
+	if((*temp)->left == NULL && (*temp)->right == NULL){ //if leaf node
+		*temp = (*temp)->parent;	
+		if((*temp)->left != NULL){
+			if((*temp)->left->x == del->x){
+				(*temp)->left = NULL;
+			}else{
+				(*temp)->right = NULL;
+			}
+		}else{
+			(*temp)->right = NULL;
+		}
+		free(del);
+		insert_fixup(rootptr,(*temp));
+		
+	}else if((*temp)->right == NULL && (*temp)->left != NULL){ //has left only child
+		*temp = (*temp)->parent;
+		if((*temp)->left->x == del->x){
+			(*temp)->left = del->left;
+			del->left->parent = (*temp);
+			*temp=(*temp)->left;
+		}else{
+			(*temp)->right = del->left;
+			del->left->parent = (*temp);
+			*temp=(*temp)->right;
+		}
+		free(del);
+		insert_fixup(rootptr,(*temp));
+	}else if((*temp)->left == NULL && (*temp)->right != NULL){ //has right only child
+		*temp = (*temp)->parent;
+		if((*temp)->right->x == del->x){
+			(*temp)->right = del->right;
+			del->right->parent = (*temp);
+			*temp=(*temp)->right;
+		}else{
+			(*temp)->left = del->right;
+			del->right->parent = (*temp);
+			*temp=(*temp)->left;
+		}
+		free(del);
+		insert_fixup(rootptr,(*temp));
+	}else if((*temp)->left != NULL && (*temp)->right != NULL){ //has 2 children
+		//find inorder successor
+		avl_node *find = (*temp)->right;
+		while(find->left != NULL){
+			find = find->left;
+		}
+		swap(&((*temp)->x),&(find->x));
+		delhelper(rootptr,&find);
+	}
+	(*temp) = *rootptr;
+}
+
+void delete(avl_node **rootptr, int *num, int size){
+	avl_node *temp = *rootptr;
+	printf("DELETE----------------------------------\n");
+	for(int i=0;i<size;i++){
+		printf("Deleting %d\n\n",num[i]);
+		while(temp->x != num[i]){
+			if(num[i] > temp->x){
+				temp = temp->right;
+			}else if(num[i] < temp->x){
+				temp = temp->left;
+			}
+		}
+		delhelper(rootptr,&temp);
+		view(*rootptr,0);
+		printf("\n----------------------------------------\n");
+	}
+
+}
+
 int main(){
 	avl_node *root = NULL;
 	int i,n=10;
+	int *del;
+	int len;
 
 	for(i=0; i<n; i++){
 		insert_value(&root,i+1);
@@ -172,4 +248,15 @@ int main(){
 	insert_value(&root,0);	
 	view(root,0);
 		printf("\n----------------------------------------\n");
+
+	printf("How many deletions: ");
+	scanf("%d",&len);
+	del = (int*)malloc(sizeof(int)*len);
+	for(int i=0;i<len;i++){
+		printf("Num ");
+		printf("%d: ",i+1);
+		scanf("%d",&(del[i]));
+	}
+	delete(&root,del,len);
+	free(del);
 }
